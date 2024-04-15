e31cf440fcf8c1f5a122a9cda7e55a4bb549bd17
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-04-14 18:56:03 -0600

    Routes system and CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+//
+// Loading routes
+//
+console.log("Loading routes...");
+const routesPath = path.join(__dirname, "routes");
+Promise.all(
+  fs.readdirSync(routesPath).map(async (filename) => {
+    try {
+      const routeFile = path.join(routesPath, filename);
+      const routeFilename = path.parse(routeFile).name;
+      const routeUrl = routeFilename == "index" ? "" : routeFilename;
+      const route = await import(routeFile);
+      app.use(`/${routeUrl}`, route.default);
+      console.log(`Route /${routeUrl} loaded correctly`);
+    } catch (error: any) {
+      console.log(
+        `Error trying to load routes file ${filename}: ${error.message}`
+      );
+    }
+  })
+).then(() => {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  });
 });
